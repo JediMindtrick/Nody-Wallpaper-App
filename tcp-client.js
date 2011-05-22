cb0ffdedace23a6e3d65e2433f93f232d3bdf0bb
brandon <brandon@TARDIS.(none)>
2011-05-22 10:42:11 -0400

    again adding logic to recursively walk directory

--- tcp-client.js
+++ tcp-client.js
@@ -13,3 +13,37 @@ conn.on('data',function(data){
 	//console.log(data.toString());
 	console.log(data);
 });
+
+var getImages = 
+function(someDir)
+{
+	var toReturn = [];
+	
+	var d = new Directory(someDir);
+	
+	var directoryList = d.listDirectories();
+	
+	for(var a = 0; a < directoryList.length; a++)
+	{
+		var toAdd = [];
+		toAdd = getImages(someDir + '/' + directoryList[a]);
+		for(var b = 0; b < toAdd.length; b++)
+		{
+			toReturn.push(toAdd[b]);
+		}
+	}
+	
+	var fileList = d.listFiles();
+	
+	for(var i = 0; i < fileList.length; i++)
+	{
+		var name = fileList[i].toLowerCase();
+		var split = name.split('.');
+		var extension = split[split.length - 1];
+		if(extension == 'png' || extension == 'jpg' || extension == 'gif')
+		{
+			toReturn.push('"' + someDir + '/' + fileList[i] + '"');
+		}
+	}
+	return toReturn;
+};
--- tcp-server.js
+++ tcp-server.js
@@ -1,16 +1,16 @@
 var count = 0;
 var net = require('net');
 var mySockets = [];
+var ranfiles = false;
 
 setInterval(function(){
 	count++;
 	console.log(count);
 	
-	/*
-	if(mySocket != null){
-		mySocket.write(count.toString() + '\n');
+	if(!ranfiles){
+		ranfiles = true;
+		getImages('/home/brandon');
 	}
-	*/
 	
 	for(var k = 0, l = mySockets.length; k < l; k++){
 		mySockets[k].write(count.toString());
@@ -26,14 +26,95 @@ var server = net.createServer(function(socket){
 		var i = mySockets.indexOf(socket);
 		mySockets.splice(i,1);
 	});	
-
-//	socket.write('hello\n');
-//	socket.write('world\n');
-
-//	socket.on('data',function(data){
-//		socket.write(data);
-//	});
 });
 
-
 server.listen(9090);
+
+var fs = require('fs');
+
+var getImages = 
+function(someDir)
+{
+	console.log('inside getImages()');
+	var toReturn = [];
+	var fileList = listFiles(someDir);
+	
+	for(var i = 0; i < fileList.length; i++)
+	{
+		var name = fileList[i].toLowerCase();
+		var split = name.split('.');
+		var extension = split[split.length - 1];
+		if(extension == 'png' || extension == 'jpg' || extension == 'gif')
+		{
+			toReturn.push('"' + someDir + '/' + fileList[i] + '"');
+		}
+	}
+	
+	var directoryList = listDirectories(someDir);
+	
+	for(var a = 0; a < directoryList.length; a++)
+	{
+		var toAdd = [];
+		toAdd = getImages(someDir + '/' + directoryList[a]);
+		for(var b = 0; b < toAdd.length; b++)
+		{
+			toReturn.push(toAdd[b]);
+		}
+	}
+	
+	
+	return toReturn;
+};
+
+var listFiles = function(someDir){
+	var toReturn = [];
+	
+	fs.readdir(someDir,function(err,files){
+        for(var i = 0, l = files.length; i < l; i++){
+       		var myStat = fs.statSync(files[i]);
+            if (myStat.isFile()) {
+				console.log(files[i]);
+				toReturn.push(files[i]);
+			}
+        }
+ 	});
+	
+	return toReturn;
+};
+
+var listDirectories = function(someDir){
+	var toReturn = [];
+	
+	fs.readdir(someDir,function(err,files){
+        for(var i = 0, l = files.length; i < l; i++){
+       		var myStat = fs.statSync(files[i]);
+            if (myStat.isDirectory()) {
+				console.log(files[i]);
+				toReturn.push(files[i]);
+			}
+        }
+ 	});
+	
+	return toReturn;
+};
+
+
+/*
+ * var myFs = require('fs');
+
+myFs.readdir('/home/brandon',function(err,files){
+        for(var i = 0, l = files.length; i < l; i++){
+                console.log(files[i]);
+                var myStat = myFs.statSync(files[i]);
+                if(myStat.isDirectory())
+                        console.log('Directory found');
+
+                if(myStat.isFile())
+                        console.log('File found');
+                console.log(myStat);
+        }
+        //console.log(files);
+ });
+
+ * 
+ */*/
